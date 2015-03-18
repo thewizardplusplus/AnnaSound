5595d4a607d3ae32d0ba4658821d87908a2756ff
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-03-19 00:22:19 +0300

    Correct the test

--- test/source/main.cpp
+++ test/source/main.cpp
@@ -7,8 +7,8 @@
 using namespace thewizardplusplus::anna::sound;
 using namespace thewizardplusplus::anna::maths;
 
-static const float RADIUS = 2.0f;
-static const float SPEED = 1e-5;
+static const float RADIUS = 10.0f;
+static const float SPEED = 3e-5;
 
 std::string GetBasePath(const std::string& app_path) {
 	std::string base_path;
@@ -20,53 +20,34 @@ std::string GetBasePath(const std::string& app_path) {
 	return base_path;
 }
 
-Sound* LoadAndPlaySoundFile(
-	SoundManager& sound_manager,
-	const std::string& filename
-) {
-	Sound* sound = sound_manager.createSound(filename);
-	sound->getSource()->setLooping(true);
-	sound->getSource()->play();
-
-	return sound;
-}
-
-void MoveSound(Sound* sound, float time) {
-	float x = RADIUS * std::cos(SPEED * time);
-	float y = RADIUS * std::sin(SPEED * time);
-	Vector3D<float> new_position(x, y, 0.0f);
-
-	sound->getSource()->setPosition(new_position);
-}
-
-void PrintSoundPosition(Sound* sound, float time) {
-	std::cout
-		<< time << ":\t("
-			<< sound->getSource()->getPosition().x << ";\t"
-			<< sound->getSource()->getPosition().y
-		<< ")" << std::endl
-		<< std::flush;
-}
-
-void ProcessError(const std::string& description) {
-	std::cerr << description << std::endl;
-}
-
 int main(int number_of_arguments, char* arguments[]) try {
 	(void)number_of_arguments;
-	std::string base_path = GetBasePath(arguments[0]) + "data/";
 
 	SoundManager sound_manager;
-	LoadAndPlaySoundFile(sound_manager, base_path + "wind.wav");
-	Sound* fire = LoadAndPlaySoundFile(sound_manager, base_path + "fire.wav");
+
+	std::string base_path = GetBasePath(arguments[0]);
+	Sound* wind = sound_manager.createSound(base_path + "data/wind.wav");
+	wind->getSource()->play();
+
+	Sound* fire = sound_manager.createSound(base_path + "data/fire.wav");
+	fire->getSource()->setLooping(true);
+	fire->getSource()->play();
 
 	float time = 0.0f;
-	while (true) {
-		MoveSound(fire, time);
-		PrintSoundPosition(fire, time);
+	while (wind->getSource()->getState() == SourceState::PLAYING) {
+		float x = RADIUS * std::cos(SPEED * time);
+		float y = RADIUS * std::sin(SPEED * time);
+		fire->getSource()->setPosition(Vector3D<float>(x, y, 0.0f));
+
+		std::cout
+			<< time << ":\t("
+				<< fire->getSource()->getPosition().x << ";\t"
+				<< fire->getSource()->getPosition().y
+			<< ")" << std::endl
+			<< std::flush;
 
 		time++;
 	}
 } catch (const std::exception& exception) {
-	ProcessError(exception.what());
+	std::cerr << exception.what() << std::endl;
 }
